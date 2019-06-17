fd498f3428812ac1d5b689adf9ce432af7bf1908
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-06-17 10:44:18 +0900

    Update messages_controller.rb
    indexアクションとcreateアクションの詳細を定義。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'groups' => 'groups#index'
+  get 'messages' => 'messages#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
