8785d2524af059fa484a5a4e72e719aedae15b3d
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-06-26 00:10:54 +0900

    Make create.json.jbuilder and so on
    jbuilderを使用し、作成したメッセージをJSON形式で返すようにした。
    
    それを、doneメソッドで受け取り、HTMLを作成し、ビューで表示できるようにした。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,32 @@
 $(function(){
+
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                     ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                     ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                     ${message.content}
+                    </p>
+                  </div>
+                </div>`
+    return html;
+  }
+
+  function scroll() {
+    $('.messages').animate({scrollTop: $('.message')[0].scrollHeight});
+  }
+
   $('#new_message').on('submit',function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +34,17 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('.form__submit').prop('disabled', false);
+      scroll()
+    })
+    .fail(function(){
+      alert('エラー');
+      $('.form__submit').prop('disabled', false);
     })
   })
 })
