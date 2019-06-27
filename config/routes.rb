8b8c7ba7a596b24c84cca8ae29ed433bcd092b4a
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-06-27 10:53:56 +0900

    Update routes.rb & users_controoler.rb
    ルーティングを設定。
    
    コントローラーのアクションを定義。
    ユーザーを検索にて取得できるようにした。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'groups' => 'groups#index'
   root 'groups#index'
-  resource :user, only: [:edit, :update]
+  resource :user, only: [:edit, :update, :index]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
