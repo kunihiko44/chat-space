0ca74c406afbff51158e4e44735677342155df80
kunihiko44 <kunihiko.yoshida.hk@gmail.com>
2019-06-28 10:06:33 +0900

    Update index.js
    検索したユーザーをJSONに変換し返す記述。
    
    ビューに追加する記述。
    
    エラー時の記述を追加。
    
    routes.rbのスペルミスを修正。

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,37 @@
 $(function() {
-  $(".chat-group-form__input").on("keyup", function() {
+
+  var search_list = $("#user-search-result");
+  
+  function appendUser(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  $("#user-search-field").on("keyup", function(e) {
+    e.preventDefault();
     var input = $("#user-search-field").val();
-  });
-});
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      }
+    })
+    .fail(function() {
+      alert('映ユーザー検索に失敗しました');
+    })
+  })
+
+})
